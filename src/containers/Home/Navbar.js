19a1142ae12f82a672c8c57afbbece94d2f1c908
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2021-09-08 13:22:32 +0700

    link in nav

--- src/containers/Home/Navbar.js
+++ src/containers/Home/Navbar.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    // <div className="sticky top-0 bg-white ">
     <div>
       <div
         style={{ height: '100px' }}
         className=" max-w-screen-lg m-auto pl-12 pr-12 pt-10 pb-5 flex flex-wrap"
       >
-        <span className="inline-block ml-auto mr-7">ABOUT</span>
-        <span className="inline-block mr-auto md:mr-7">WORKS</span>
+        <a href="#about" className="inline-block ml-auto mr-7">ABOUT</a>
+        <a href="#works" className="inline-block mr-auto md:mr-7">WORKS</a>
       </div>
     </div>
   );
--- src/containers/Home/Works.js
+++ src/containers/Home/Works.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Works = () => {
   return (
-    <div id="work" className="max-w-screen-lg m-auto px-12 py-24">
+    <div id="works" className="max-w-screen-lg m-auto px-12 py-24">
       <h2 className="text-2xl font-bold">WORKS</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
         <WorkCard
